e7c1a5b686c87caced153f29e17a5d2dde44d027
Elaine2299 <elainefedes220199@gmail.com>
2025-05-28 20:31:59 +0530

    Develop Add

--- src/test/java/testcases/BaseTest.java
+++ src/test/java/testcases/BaseTest.java
@@ -127,6 +127,7 @@ public class BaseTest {
     public void loginSuccess(){
         System.out.println("User Logged in Successfully Great!");
         System.out.println("Commit successful");
+        System.out.println("Develop Branch");
     }
 
     @DataProvider
